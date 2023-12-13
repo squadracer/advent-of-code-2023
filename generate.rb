3907291df7a11d6cca73e46bf3f136fa62d9b06e
Théo Dupuis <theo@squadracer.com>
2023-12-13 11:30:59 +0100

    take the date by default

--- generate.rb
+++ generate.rb
@@ -1,12 +1,13 @@
 require 'fileutils'
-day =  ARGV.first.rjust(2,'0')
+require 'date'
+day = ARGV.first&.rjust(2,'0') || Date.today.day.to_s.rjust(2,'0')
 file = File.open("days/template/dayXX.rb")
 target_folder = Dir.mkdir("days/day#{day}")
 folder = "days/template"
 filenames = Dir.entries(folder) - ['.','..']
 filenames.each do |filename|
-    file = File.open("days/template/#{filename}")
-    file_data = file.read.gsub('XX',day)
-    File.open("days/day#{day}/#{filename.gsub('XX',day)}", "w") { |f| f.write file_data }
+  file = File.open("days/template/#{filename}")
+  file_data = file.read.gsub('XX',day)
+  File.open("days/day#{day}/#{filename.gsub('XX',day)}", "w") { |f| f.write file_data }
 end
 File.rename "days/day#{day}/day#{day}_spec.rb", "spec/day#{day}_spec.rb"
